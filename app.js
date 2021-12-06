8bc5ae70cf6f13077912acf555a28fa8ed4ccf45
Rick Johnson <carl4917@umn.edu>
2021-12-05 22:07:14 -0600

    generating promises

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 const inquirer = require('inquirer');
 
@@ -131,12 +131,22 @@ const promptUser = () => {
       });
   };
 
+
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-     const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-       if (err) throw new Error(err);
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
