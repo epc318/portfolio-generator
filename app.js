998e1510b5137d4460998b5e93d5e14681b47240
Rick Johnson <carl4917@umn.edu>
2021-12-05 20:01:45 -0600

    footer and header

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 const inquirer = require('inquirer');
+
+
 const promptUser = () => {
     return inquirer.prompt([
       {
@@ -130,16 +134,9 @@ const promptUser = () => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+     const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+    });
   });
-
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
-
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('./index.html', pageHTML, err => {
-//  if (err) throw err;
-
-//  console.log('Portfolio complete! Check out index.html to see the output!');
-//});
